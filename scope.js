6cf39fe9d555deaa631be7d69d2930efd765f661
HarrisAmin <harrisaminjutt@gmail.com>
2026-05-16 13:20:11 +0500

    Scope mechanics

---
+++ scope.js
@@ -0,0 +1,23 @@
+var x = 23;
+// Global object
+// let and const can be accessed anywhere in the script but hidden bu global object
+
+function myFunc() {
+  if (true) {
+    var a = 10;
+    let b = 20;
+  }
+  console.log("a: ", a);
+  // console.log("b: ", b);  Reference Error
+  var y = 10;
+}
+
+myFunc();
+
+// Variables declared with var are limited to whole function no matter in which block they are defined
+// Variables declared with let are only limited to the block scope
+
+// Global object in browser is window
+// Global object in terminal is global
+
+// Any var variable declared within root becomes global variable
